c9f451b30b3b95a410f3048d6a8fa668308dc602
yerin1562 <yerin1562@gmail.com>
2025-01-16 14:37:15 +0900

    add abstract base model

--- tabom/models.py
+++ tabom/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
-# Create your models here.
 
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
+class BaseModel(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel):
+    name = models.CharField(max_length=50)
     # audit: updates, created
 
 
-class Article(models.Model):
+class Article(BaseModel):
     title = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Like(models.Model):
+class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
